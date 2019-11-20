f270cf2f9d876da68b3b318251ff47252cacf3cf
Nelson Huang <xinchin@gmail.com>
2019-11-20 17:24:19 +0800

    測試檔案上傳

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Faker\Provider\Image;
 use Illuminate\Http\Request;
 
 /*
@@ -15,4 +16,18 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+// https://laracasts.com/discuss/channels/laravel/how-to-save-upload-image-to-directory-in-laravel-58?page=1
+
+Route::post('/mytest', function(Request $request){
+
+    if ($request->hasFile('image')) {
+        $image      = $request->file('image');
+        $fileName   = time() . '.' . $image->getClientOriginalExtension();
+
+        $image->move(public_path().'/images/', $fileName);
+        return $request;
+    }
 });
